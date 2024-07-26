c3c2488ca6ad86d36fb626a10269c208c57d07bd
Fabian Döz <fabian@doez.info>
2024-07-26 09:59:30 +0200

    Initial commit

---
+++ src/main/java/de/webdev/recap4_spring/Recap4SpringApplication.java
@@ -0,0 +1,13 @@
+package de.webdev.recap4_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recap4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recap4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/webdev/recap4_spring/Recap4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.webdev.recap4_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recap4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
